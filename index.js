d6fe63db1112436ed7631b38c759bc2d4b886291
Jean MOREAU <jean.moreau@nordnet.net>
2022-09-17 11:32:18 +0200

    add transition and reactive cursor

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ var currentPlay = 0
 var videos;
 
 const play = function () {
-    videos = document.querySelectorAll("video");
-    videos[1].style.display = "none";
-    videos[2].style.display = "none";
-    videos[3].style.display = "none";
-    console.info("test ");
-    // Get all videos.
-
+    this.videos = document.querySelectorAll("video");
+    this.input = document.getElementsByTagName('input');
+    this.videos[1].style.opacity = 0;
+    this.videos[2].style.opacity = 0;
+    this.videos[3].style.opacity = 0;
     // Create a promise to wait all videos to be loaded at the same time.
     // When all of the videos are ready, call resolve().
     var promise = new Promise(function (resolve) {
@@ -34,15 +32,19 @@ const play = function () {
 
     // Play all videos one by one only when all videos are ready to be played.
     promise.then(function () {
-        console.info("done");
+        videos[1].muted = true;
+        videos[2].muted = true;
+        videos[3].muted = true;
 
-        videos.forEach(function (v, index) {
-            if(index > 0){
-                v.muted = true;
-            }
-            v.loop = true;
-            v.play();
-        });
+        videos[0].loop = true;
+        videos[1].loop = true;
+        videos[2].loop = true;
+        videos[3].loop = true;
+
+        videos[1].play()
+        videos[2].play()
+        videos[3].play()
+        videos[0].play()
     });
 }
 
@@ -50,4 +52,20 @@ const switchTo = function (bool) {
     videos[bool ? currentPlay + 1 : currentPlay - 1].style.display = "block";
     currentPlay = bool ? currentPlay + 1 : currentPlay - 1;
     videos[bool ? currentPlay - 1 : currentPlay + 1].style.display = "none";
+}
+
+const switchVideo = function (value) {
+    document.getElementById("labelCurseur").innerText = +value;
+    const values = value.toString().split(".");
+    console.log(value)
+    this.videos.forEach((video, index) => {
+        // console.log(index," !== ",values[0])
+        this.videos[+values[0]].style.opacity = Math.abs((+values[1] / 10) - 1);
+        this.videos[+values[0] + 1].style.opacity = (+values[1] / 10);
+        if (index !== +values[0]) {
+            this.videos[index].style.opacity = 0;
+        }
+    });
+
+
 }
